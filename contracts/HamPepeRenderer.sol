cb14554970a8e04d3e4274340740590f6a8cefd2
based-bren <alphabrendarious@gmail.com>
2024-06-23 19:27:03 +0100

    Start project

---
+++ contracts/HamPepeRenderer.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "utils/SSTORE2.sol";
+import "utils/LibString.sol";
+import "auth/Owned.sol";
+import "utils/Base64.sol";
+import "contracts/HamPepeTraits.sol";
