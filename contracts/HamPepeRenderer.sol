2718305414d2b807dbd67d9190fa67d4debb2339
based-bren <alphabrendarious@gmail.com>
2024-06-23 22:01:53 +0100

    Rendered finished

--- contracts/HamPepeRenderer.sol
+++ contracts/HamPepeRenderer.sol
@@ -7,3 +7,26 @@ import "utils/LibString.sol";
 import "auth/Owned.sol";
 import "utils/Base64.sol";
 import "contracts/HamPepeTraits.sol";
+
+contract HamPepeRenderer is Owned {
+    using LibString for uint256;
+
+    HamPepeTraits traitsMetaData;
+
+    address public traitsImagePointer;
+    string description = 
+    "1000 based Pepes living on Ham Chain L3";
+
+    error TraitsImageAlreadySet ();
+
+    constructor(HamPepeTraits _traitsMetaData) Owned(msg.sender) {
+        traitsMetaData = _traitsMetaData;
+    }
+
+    function setTraitsImage(string calldata data) external onlyOwner {
+        if (traitsImagePointer != address(0)) {
+            revert TraitsImageAlreadySet();
+        }
+        traitsImagePointer = SSTORE2.write(bytes(data));
+    }
+}
